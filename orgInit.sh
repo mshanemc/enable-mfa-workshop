11b0c63a18956b827e1977b16979835aa645b3fc
mshanemc <shane.mclaughlin@salesforce.com>
2020-03-20 10:35:34 -0500

    init, config

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,5 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json --userprefix int -o work.shop
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json --userprefix 2fa -o work.shop
 sfdx force:source:push
 sfdx force:user:permset:assign -n CustomerID
-sfdx shane:user:password:set -l User -g User -p sfdx1234 --json
-sfdx force:apex:execute -f scripts/CustomerIDSetup.cls
+sfdx shane:user:password:set -l User -g User -p letsAll2FA --json
 sfdx force:org:open
