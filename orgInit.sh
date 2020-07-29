ff9a1d9b8785573e9b5ae68cc59ebb7fa0361e93
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-29 11:29:28 -0500

    password change

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json --userprefix 2fa -o work.shop
 sfdx force:source:push
 sfdx force:user:permset:assign -n CustomerID
-sfdx shane:user:password:set -l User -g User -p letsAll2FA --json
+sfdx shane:user:password:set -l User -g User -p LetsAllMFA --json
 sfdx force:org:open
